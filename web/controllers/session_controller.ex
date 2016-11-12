a3934cdf07cf3bf9d3b52b53ca880787bbe46689
Fajar M Firdaus <fajarmf@gmail.com>
2016-11-13 04:19:15 +0700

    fix empty login bug

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -2,16 +2,22 @@ defmodule Koalog.SessionController do
   use Koalog.Web, :controller
 
   alias Koalog.{Repo, User}
-  import Comeonin.Bcrypt, only: [checkpw: 2]
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
   plug :scrub_params, "user" when action in [:create]
 
   def new(conn, _params) do
     render conn, "new.html", changeset: User.changeset(%User{})
   end
 
-  def create(conn, %{"user" => user_params}) do
-    Repo.get_by(User, username: user_params["username"])
-    |> sign_in(user_params["password"], conn)
+  def create(conn, %{"user" => %{"username" => username, "password" => password}})
+  when not is_nil(username) and not is_nil(password) do
+    user = Repo.get_by(User, username: username)
+    sign_in(user, password, conn)
+  end
+
+  def create(conn, _) do
+    failed_login(conn)
   end
 
   def delete(conn, _params) do
@@ -21,10 +27,17 @@ defmodule Koalog.SessionController do
     |> redirect(to: page_path(conn, :index))
   end
 
-  defp sign_in(user, password, conn) when is_nil(user) do
+  defp failed_login(conn) do
+    dummy_checkpw()
     conn
+    |> put_session(:current_user, nil)
     |> put_flash(:error, "Invalid username/password combination!")
     |> redirect(to: page_path(conn, :index))
+    |> halt()
+  end
+
+  defp sign_in(user, _password, conn) when is_nil(user) do
+    failed_login(conn)
   end
 
   defp sign_in(user, password, conn) do
@@ -34,10 +47,7 @@ defmodule Koalog.SessionController do
       |> put_flash(:info, "Sign in successful!")
       |> redirect(to: page_path(conn, :index))
     else
-      conn
-      |> put_session(:current_user, nil)
-      |> put_flash(:error, "Invalid username/password combination!")
-      |> redirect(to: page_path(conn, :index))
+      failed_login(conn)
     end
   end
 end
