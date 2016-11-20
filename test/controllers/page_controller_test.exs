6cfb6dec1cc4d79c53bd91921c6f773fa7b6eec3
Fajar M Firdaus <fajarmf@gmail.com>
2016-11-21 05:53:16 +0700

    show project koala id ads on the bottom of landing page

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -4,5 +4,6 @@ defmodule Koalog.PageControllerTest do
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Recommended for you"
+    assert html_response(conn, 200) =~ "Project Koala ID Blog"
   end
 end
