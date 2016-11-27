a45a5e370dd54b738587ae99840e5de9027573e8
Fajar M Firdaus <fajarmf@gmail.com>
2016-11-27 09:13:24 +0700

    site header

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -4,6 +4,6 @@ defmodule Koalog.PageControllerTest do
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Recommended for you"
-    assert html_response(conn, 200) =~ "Project Koala ID Blog"
+    assert html_response(conn, 200) =~ "Project Koala - Konten Anak Lokal"
   end
 end
