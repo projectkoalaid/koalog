816e80839f3451bac6ec6654d637b3be90a998f2
Fajar M Firdaus <fajarmf@gmail.com>
2016-12-24 07:15:29 +0700

    fix link and display on home page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,7 +6,7 @@ defmodule Koalog.PageController do
 	alias Koalog.Post
 
   def index(conn, _params) do
-  	featured_posts = Post.featured(4) |> Repo.all |> Repo.preload([:user])
+  	featured_posts = Post.featured(8) |> Repo.all |> Repo.preload([:user])
   	conn
   	|> assign(:css, "/css/recipe.css")
   	# |> assign(:css, "http://css01.media-allrecipes.com/assets/deployables/v-1.45.0.2674/main-css.bundled.Css")
